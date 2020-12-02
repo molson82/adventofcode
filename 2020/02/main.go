48abdea88a7a399fb3cb9175edcb239884cb1fef
molson82 <wvandebelt82@yahoo.com>
2020-12-02 02:20:55 -0500

    day 2 part 2 done

--- 2020/02/main.go
+++ 2020/02/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 func check(err error) {
@@ -14,7 +15,7 @@ func check(err error) {
 	}
 }
 
-func readInput(file string) int {
+func readInput(file string) (int, int) {
 
 	f, err := os.Open(file)
 	defer f.Close()
@@ -22,16 +23,20 @@ func readInput(file string) int {
 
 	scanner := bufio.NewScanner(f)
 	var count int
+	var count2 int
 	for scanner.Scan() {
 		line := scanner.Text()
 		lineSplit := strings.Split(line, ":")
-		key := strings.Trim(lineSplit[0], " ")
-		value := strings.Trim(lineSplit[1], " ")
-		if validate(key, value) {
+		policy := strings.Trim(lineSplit[0], " ")
+		pass := strings.Trim(lineSplit[1], " ")
+		if validate(policy, pass) {
 			count++
 		}
+		if tobogganValidate(policy, pass) {
+			count2++
+		}
 	}
-	return count
+	return count, count2
 }
 
 func validate(key string, pass string) bool {
@@ -51,16 +56,34 @@ func validate(key string, pass string) bool {
 	return false
 }
 
+func tobogganValidate(key string, pass string) bool {
+	keySplit := strings.Split(key, " ")
+	hl := strings.Split(keySplit[0], "-")
+	low, err := strconv.Atoi(hl[0])
+	check(err)
+	high, err2 := strconv.Atoi(hl[1])
+	check(err2)
+	char, _ := utf8.DecodeLastRuneInString(keySplit[1])
+	passRune := []rune(pass)
+
+	// xor
+	return (passRune[low-1] == char || passRune[high-1] == char) && passRune[low-1] != passRune[high-1]
+}
+
 /*
 Part 1
  - attempt 1 = 459 | wrong
 		- Using a map made it so there were keys getting overridden, since there are duplicates in the input
  - attempt 2 = 586 | correct
 Part 2
- - attempt 1 =
+ - attempt 1 = 143 | wrong
+		- had a bad xor
+ - attempt 2 = 352 | correct
 */
 func main() {
+	total1, total2 := readInput("input.txt")
 	fmt.Println("Day 2 Part 1 ---------------------------------")
-	total := readInput("input.txt")
-	fmt.Printf("Total correct passwords: %v\n", total)
+	fmt.Printf("Total correct passwords: %v\n", total1)
+	fmt.Printf("\nDay 2 Part 2 ---------------------------------\n")
+	fmt.Printf("Total correct passwords: %v\n", total2)
 }
