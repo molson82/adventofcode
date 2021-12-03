ccca94aff43af72175200d2f212dcd41f1a6e6bd
molson82 <wvandebelt82@yahoo.com>
2021-12-03 14:41:38 -0500

    :sparkles: day 3 part 1 & 2 done

--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/molson82/adventofcode/internal/2021/day2"
+	"github.com/molson82/adventofcode/internal/2021/day3"
 	"github.com/molson82/adventofcode/pkg/utils"
 )
 
 func main() {
 	log.Println("Advent of Code")
 
-	// attempt 1: 2039912 = correct
-	log.Printf("2021 Day 2 Part 1: %v", day2.Part1(utils.ReadInput("inputs/day2_input.txt")))
-	// attempt 1: 1942068080 = correct
-	log.Printf("2021 Day 2 Part 2: %v", day2.Part2(utils.ReadInput("inputs/day2_input.txt")))
+	log.Printf("Part 2: %v", day3.Part2(utils.ReadInput("inputs/day3_input.txt")))
 }
