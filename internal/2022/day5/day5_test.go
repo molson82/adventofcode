7c89a2d90ce91ce1c2af6f7cf7b769d2f9d994ff
William-Vandebelt <wvandebelt@salesforce.com>
2022-12-05 18:14:11 -0500

    day 5

--- internal/2022/day5/day5_test.go
+++ internal/2022/day5/day5_test.go
@@ -14,16 +14,30 @@ const (
 var _ = Describe("Day0", func() {
 
 	Context("Part 1 Sample", func() {
-		It("should equal 0", func() {
+		It("should equal CMZ", func() {
 			input := utils.ReadInput(sampleFile)
-			Expect(Part1(input)).To(Equal(0))
+			Expect(Part1(input)).To(Equal("CMZ"))
+		})
+	})
+
+	Context("Part 1 Input", func() {
+		It("should equal LJSVLTWQM", func() {
+			input := utils.ReadInput(inputFile)
+			Expect(Part1(input)).To(Equal("LJSVLTWQM"))
 		})
 	})
 
 	Context("Part 2 Sample", func() {
-		It("should equal 0", func() {
+		It("should equal MCD", func() {
 			input := utils.ReadInput(sampleFile)
-			Expect(Part2(input)).To(Equal(0))
+			Expect(Part2(input)).To(Equal("MCD"))
+		})
+	})
+
+	Context("Part 2 Input", func() {
+		It("should equal BRQWDBBJM", func() {
+			input := utils.ReadInput(inputFile)
+			Expect(Part2(input)).To(Equal("BRQWDBBJM"))
 		})
 	})
 })
