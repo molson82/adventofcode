ab7a1cd4697c778510b90417a14c06e41d6f69bc
William-Vandebelt <wvandebelt@salesforce.com>
2022-12-05 14:16:33 -0500

    2022 | day3

--- internal/2022/day3/day3_test.go
+++ internal/2022/day3/day3_test.go
@@ -14,16 +14,30 @@ const (
 var _ = Describe("Day1", func() {
 
 	Context("Part 1 Sample", func() {
-		It("should equal 0", func() {
+		It("should equal 157", func() {
 			input := utils.ReadInput(sampleFile)
-			Expect(Part1(input)).To(Equal(0))
+			Expect(Part1(input)).To(Equal(157))
 		})
 	})
 
 	Context("Part 1 Input", func() {
-		It("should not equal 0", func() {
+		It("should equal 8018", func() {
 			input := utils.ReadInput(inputFile)
-			Expect(Part1(input)).To(Equal(0))
+			Expect(Part1(input)).To(Equal(8018))
+		})
+	})
+
+	Context("Part 2 Sample", func() {
+		It("should equal 70", func() {
+			input := utils.ReadInput(sampleFile)
+			Expect(Part2(input)).To(Equal(70))
+		})
+	})
+
+	Context("Part 2 Input", func() {
+		It("should equal 2518", func() {
+			input := utils.ReadInput(inputFile)
+			Expect(Part2(input)).To(Equal(2518))
 		})
 	})
 })
--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/molson82/adventofcode/internal/2022/day2"
+	"github.com/molson82/adventofcode/internal/2022/day3"
 	"github.com/molson82/adventofcode/pkg/utils"
 )
 
 func main() {
 	log.Println("Advent of Code")
 
-	log.Printf("Answer: %v", day2.Part2(utils.ReadInput("internal/2022/day2/input.txt")))
+	log.Printf("Answer: %v", day3.Part2(utils.ReadInput("internal/2022/day3/input.txt")))
 }
