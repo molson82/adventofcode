110028130e5b12c5f60c2859bde00d7b2b169c5b
William-Vandebelt <wvandebelt@salesforce.com>
2022-12-05 18:18:50 -0500

    day 6 prep

---
+++ internal/2022/day6/day6.go
@@ -0,0 +1,9 @@
+package day6
+
+func Part1(input []string) int {
+	return 0
+}
+
+func Part2(input []string) int {
+	return 0
+}
---
+++ internal/2022/day6/day6_suite_test.go
@@ -0,0 +1,13 @@
+package day6
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestDay2(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Day0 Suite")
+}
