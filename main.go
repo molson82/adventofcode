cb102d6bb379663a73b6e89a923ddec1de746353
William-Vandebelt <wvandebelt@salesforce.com>
2022-12-05 15:41:57 -0500

    day 4

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/molson82/adventofcode/internal/2022/day3"
+	"github.com/molson82/adventofcode/internal/2022/day4"
 	"github.com/molson82/adventofcode/pkg/utils"
 )
 
 func main() {
 	log.Println("Advent of Code")
 
-	log.Printf("Answer: %v", day3.Part2(utils.ReadInput("internal/2022/day3/input.txt")))
+	log.Printf("Answer: %v", day4.Part1(utils.ReadInput("internal/2022/day4/input.txt")))
 }
