d589d217a28cf1aedc4fbe4b1b76ccb048d5411c
William Vandebelt <will@Williams-Mac-mini.local>
2022-12-06 01:21:23 -0500

    day 6 part 1

--- internal/2022/day6/day6.go
+++ internal/2022/day6/day6.go
@@ -1,6 +1,27 @@
 package day6
 
-func Part1(input []string) int {
+import (
+	"fmt"
+	"strings"
+)
+
+func Part1(input string) int {
+	var seg string
+OUTER:
+	for i := 0; i < len(input); i++ {
+		for j := i; j < len(input); j++ {
+			s := string(input[j])
+			if strings.Contains(seg, s) {
+				seg = ""
+				continue OUTER
+			}
+			seg = fmt.Sprintf("%s%s", seg, s)
+			if len(seg) == 4 {
+				return j + 1
+			}
+		}
+	}
+
 	return 0
 }
 
--- internal/2022/day6/day6_test.go
+++ internal/2022/day6/day6_test.go
@@ -11,12 +11,31 @@ const (
 	sampleFile = "./sample.txt"
 )
 
-var _ = Describe("Day0", func() {
+type input struct {
+	Input  string
+	Result int
+}
+
+var _ = Describe("Day6", func() {
 
 	Context("Part 1 Sample", func() {
-		It("should equal 0", func() {
-			input := utils.ReadInput(sampleFile)
-			Expect(Part1(input)).To(Equal(0))
+		It("should equal list of inputs", func() {
+			inputs := []input{
+				{Input: "bvwbjplbgvbhsrlpgdmjqwftvncz", Result: 5},
+				{Input: "nppdvjthqldpwncqszvftbrmjlhg", Result: 6},
+				{Input: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", Result: 10},
+				{Input: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", Result: 11},
+			}
+			for _, v := range inputs {
+				Expect(Part1(v.Input)).To(Equal(v.Result))
+			}
+		})
+	})
+
+	Context("Part 1 Input", func() {
+		It("should equal 1766", func() {
+			input := utils.ReadInput(inputFile)
+			Expect(Part1(input[0])).To(Equal(1766))
 		})
 	})
 
