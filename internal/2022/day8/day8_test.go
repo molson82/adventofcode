c95ee8e47c413b9c8643219639acd47442187241
William Vandebelt <will@Williams-Mac-mini.local>
2022-12-08 02:22:32 -0500

    day 8 setup

---
+++ internal/2022/day8/day8_test.go
@@ -0,0 +1,29 @@
+package day8
+
+import (
+	"github.com/molson82/adventofcode/pkg/utils"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	inputFile  = "./input.txt"
+	sampleFile = "./sample.txt"
+)
+
+var _ = Describe("Day8", func() {
+
+	Context("Part 1 Sample", func() {
+		It("should equal 21", func() {
+			input := utils.ReadInput(sampleFile)
+			Expect(Part1(input)).To(Equal(21))
+		})
+	})
+
+	Context("Part 2 Sample", func() {
+		It("should equal 0", func() {
+			input := utils.ReadInput(sampleFile)
+			Expect(Part2(input)).To(Equal(0))
+		})
+	})
+})
