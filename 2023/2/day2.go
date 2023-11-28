621e047311771a2508c350ad787c47840616ef3f
molson82 <wvandebelt82@yahoo.com>
2023-11-27 22:08:41 -0500

    :tada: :rocket:

---
+++ 2023/2/day2.go
@@ -0,0 +1,15 @@
+package day2
+
+import "fmt"
+
+func Part1() {
+	var ans int
+	// code here...
+	fmt.Println("2023/2 part1 ans: ", ans)
+}
+
+func Part2() {
+	var ans int
+	// code here...
+	fmt.Println("2023/2 part2 ans: ", ans)
+}
