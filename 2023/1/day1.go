e1a1848ef73049ea4a56be3a0704843f2ca1380b
molson82 <wvandebelt82@yahoo.com>
2023-12-01 01:13:29 -0500

    2 ⭐

--- 2023/1/day1.go
+++ 2023/1/day1.go
@@ -2,16 +2,107 @@ package day1
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 )
 
-func Part1() {
+func Part1(lines []string) int {
 	var ans int
-	// code here...
+	for _, v := range lines {
+		lineNum := getLineNum(v)
+		num, err := strconv.ParseInt(lineNum, 10, 0)
+		if err != nil {
+			log.Fatalln("err: ", err)
+		}
+		ans += int(num)
+	}
 	fmt.Println("2023/1 part1 ans: ", ans)
+	return ans
 }
 
-func Part2() {
+func Part2(lines []string) int {
 	var ans int
-	// code here...
+	for _, v := range lines {
+		lineNum := GetLineNumV2(v)
+		num, err := strconv.ParseInt(lineNum, 10, 0)
+		if err != nil {
+			log.Fatalln("err: ", err)
+		}
+		ans += int(num)
+	}
+
 	fmt.Println("2023/1 part2 ans: ", ans)
+	return ans
+}
+
+func GetLineNumV2(str string) string {
+	var lineNum string
+	var word string
+	var lastChar string
+	for _, c := range str {
+		lastChar = string(c)
+		word = fmt.Sprintf("%s%s", word, string(c))
+		// check if it's a spelled out number
+		if sn := spelledNumToInt(word); sn != "" {
+			lineNum = fmt.Sprintf("%s%s", lineNum, sn)
+			word = lastChar
+		}
+		// check if it's a valid number
+		_, err := strconv.ParseInt(string(c), 10, 0)
+		if err != nil {
+			continue
+		} else {
+			lineNum = fmt.Sprintf("%s%s", lineNum, string(c))
+			word = lastChar
+		}
+	}
+	lineNum = fmt.Sprintf("%s%s", string(lineNum[0]), string(lineNum[len(lineNum)-1]))
+	return lineNum
+}
+
+func getLineNum(str string) string {
+	var lineNum string
+	for _, c := range str {
+		_, err := strconv.ParseInt(string(c), 10, 0)
+		if err != nil {
+			continue
+		} else {
+			lineNum = fmt.Sprintf("%s%s", lineNum, string(c))
+		}
+	}
+	lineNum = fmt.Sprintf("%s%s", string(lineNum[0]), string(lineNum[len(lineNum)-1]))
+	return lineNum
+}
+
+func spelledNumToInt(str string) string {
+	if strings.Contains(str, "one") {
+		return "1"
+	}
+	if strings.Contains(str, "two") {
+		return "2"
+	}
+	if strings.Contains(str, "three") {
+		return "3"
+	}
+	if strings.Contains(str, "four") {
+		return "4"
+	}
+	if strings.Contains(str, "five") {
+		return "5"
+	}
+	if strings.Contains(str, "six") {
+		return "6"
+	}
+	if strings.Contains(str, "seven") {
+		return "7"
+	}
+	if strings.Contains(str, "eight") {
+		return "8"
+	}
+	if strings.Contains(str, "nine") {
+		return "9"
+	}
+
+	return ""
 }
--- 2023/1/day1_test.go
+++ 2023/1/day1_test.go
@@ -3,9 +3,52 @@ package day1_test
 import (
 	"testing"
 
+	day1 "github.com/molson82/adventofcode/2023/1"
+	"github.com/molson82/adventofcode/pkg/utils"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestPart1Sample(t *testing.T) {
+	sample := utils.ReadInput("./sample.txt")
+	expect := day1.Part1(sample)
+	assert.Equal(t, expect, 142)
+}
+
 func TestPart1(t *testing.T) {
-	assert.True(t, true, "true is true")
+	input := utils.ReadInput("./input.txt")
+	expect := day1.Part1(input)
+	assert.Equal(t, expect, 55172)
+}
+
+func TestPart2Sample(t *testing.T) {
+	sample := utils.ReadInput("./samplePart2.txt")
+	expect := day1.Part2(sample)
+	assert.Equal(t, expect, 281)
+}
+
+func TestPart2(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	expect := day1.Part2(input)
+	assert.NotEqual(t, expect, 54953)
+	assert.Equal(t, expect, 54925)
+}
+
+func TestGetLineNumV2(t *testing.T) {
+	var testInput = [][]string{
+		{"pknhg9jql51one4", "94"},
+		{"1threetwo6rfvsgjbhjlcfour", "14"},
+		{"four7fiveseven3fourdqvsnkkdrbrhnfsx", "44"},
+		{"7rcqcbqsfz766", "76"},
+		{"4hdzzzggfzs18eight2twoqmnlcxzxs", "42"},
+		{"x8", "88"},
+		{"1two3fourxxx5xxxsixseven", "17"},
+		{"xpkfldsggg3eightbqtbrfthd", "38"},
+		{"eighthree", "83"},
+		{"sevenine", "79"},
+	}
+
+	for _, v := range testInput {
+		e := day1.GetLineNumV2(v[0])
+		assert.Equal(t, e, v[1])
+	}
 }
