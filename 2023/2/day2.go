766cd23b23dfbf57821db9553f8284821ed503d1
molson82 <wvandebelt82@yahoo.com>
2023-12-02 01:03:58 -0500

    4 ⭐

--- 2023/2/day2.go
+++ 2023/2/day2.go
@@ -1,15 +1,94 @@
 package day2
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 
-func Part1() {
+	"github.com/molson82/adventofcode/pkg/utils"
+)
+
+var (
+	redCount   int = 12
+	greenCount int = 13
+	blueCount  int = 14
+)
+
+func Part1(lines []string) int {
 	var ans int
-	// code here...
-	fmt.Println("2023/2 part1 ans: ", ans)
+
+lines:
+	for _, g := range lines {
+		drawings := strings.Split(g, ": ")
+		gameNum := strings.Split(drawings[0], "Game ")[1]
+		n := utils.Str_to_Int(gameNum)
+
+		sets := strings.Split(drawings[1], "; ")
+		for _, s := range sets {
+			if !checkPairs(s) {
+				continue lines
+			}
+		}
+		ans += n
+	}
+	return ans
 }
 
-func Part2() {
+func Part2(lines []string) int {
 	var ans int
-	// code here...
-	fmt.Println("2023/2 part2 ans: ", ans)
+
+	for _, g := range lines {
+		drawings := strings.Split(g, ": ")
+		var maxBlue, maxRed, maxGreen int
+
+		splitFunc := func(c rune) bool {
+			return c == ',' || c == ';'
+		}
+		for _, pair := range strings.FieldsFunc(drawings[1], splitFunc) {
+			p := strings.Trim(pair, " ")
+			count := utils.Str_to_Int(strings.Split(p, " ")[0])
+			color := strings.Trim(strings.Split(p, " ")[1], ";")
+			if color == "blue" && count > maxBlue {
+				maxBlue = count
+			}
+			if color == "red" && count > maxRed {
+				maxRed = count
+			}
+			if color == "green" && count > maxGreen {
+				maxGreen = count
+			}
+		}
+		power := maxBlue * maxRed * maxGreen
+		ans += power
+
+	}
+
+	return ans
+}
+
+func checkPairs(input string) bool {
+	pattern := `(\d+)\s+(\w+)`
+	regex := regexp.MustCompile(pattern)
+	matches := regex.FindAllStringSubmatch(input, -1)
+	for _, match := range matches {
+		count, err := strconv.Atoi(match[1])
+		if err != nil {
+			fmt.Printf("Error converting count: %v\n", err)
+			continue
+		}
+
+		color := match[2]
+
+		if color == "blue" && count > blueCount {
+			return false
+		}
+		if color == "red" && count > redCount {
+			return false
+		}
+		if color == "green" && count > greenCount {
+			return false
+		}
+	}
+	return true
 }
--- 2023/2/day2_test.go
+++ 2023/2/day2_test.go
@@ -3,9 +3,31 @@ package day2_test
 import (
 	"testing"
 
+	day2 "github.com/molson82/adventofcode/2023/2"
+	"github.com/molson82/adventofcode/pkg/utils"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestPart1Sample(t *testing.T) {
+	sample := utils.ReadInput("./sample.txt")
+	ans := day2.Part1(sample)
+	assert.Equal(t, ans, 8)
+}
+
 func TestPart1(t *testing.T) {
-	assert.True(t, true, "true is true")
+	input := utils.ReadInput("./input.txt")
+	ans := day2.Part1(input)
+	assert.Equal(t, ans, 2256)
+}
+
+func TestPart2Sample(t *testing.T) {
+	sample := utils.ReadInput("./sample.txt")
+	ans := day2.Part2(sample)
+	assert.Equal(t, ans, 2286)
+}
+
+func TestPart2(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	ans := day2.Part2(input)
+	assert.Equal(t, ans, 74229)
 }
