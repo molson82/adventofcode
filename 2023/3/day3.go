90a2fc31a3822f0cbe69424a28b32a17cdb87851
molson82 <wvandebelt82@yahoo.com>
2023-12-03 14:43:34 -0500

    refactor

--- 2023/3/day3.go
+++ 2023/3/day3.go
@@ -8,110 +8,9 @@ import (
 	"github.com/molson82/adventofcode/pkg/utils"
 )
 
-func Part1(lines []string) int {
-	ans = 0
-	top = 0
-	right = len(lines[0]) - 1
-	left = 0
-	bottom = len(lines) - 1
-
-	validNumbers := make(map[struct {
-		x int
-		y int
-	}]int)
-	for i, l := range lines {
-		for j, c := range l {
-			if !unicode.IsDigit(c) && c != '.' {
-				// fmt.Println("cursor: ", string(c))
-				if i >= top && i != bottom {
-					if CheckBelow(i, j, lines) {
-						start, num, err := GetNumber(j, lines[i+1])
-						// fmt.Println("num: ", num)
-						utils.CheckErr(err)
-						validNumbers[struct {
-							x int
-							y int
-						}{i + 1, start}] = num
-					}
-					if (j >= left && j != right) && CheckDownRight(i, j, lines) {
-						start, num, err := GetNumber(j+1, lines[i+1])
-						// fmt.Println("num: ", num)
-						utils.CheckErr(err)
-						validNumbers[struct {
-							x int
-							y int
-						}{i + 1, start}] = num
-					}
-					if (j <= right && j != left) && CheckDownLeft(i, j, lines) {
-						start, num, err := GetNumber(j-1, lines[i+1])
-						// fmt.Println("num: ", num)
-						utils.CheckErr(err)
-						validNumbers[struct {
-							x int
-							y int
-						}{i + 1, start}] = num
-					}
-				}
-				if i <= bottom && i != top {
-					if CheckAbove(i, j, lines) {
-						start, num, err := GetNumber(j, lines[i-1])
-						// fmt.Println("num: ", num)
-						utils.CheckErr(err)
-						validNumbers[struct {
-							x int
-							y int
-						}{i - 1, start}] = num
-					}
-					if (j >= left && j != right) && CheckUpRight(i, j, lines) {
-						start, num, err := GetNumber(j+1, lines[i-1])
-						// fmt.Println("num: ", num)
-						utils.CheckErr(err)
-						validNumbers[struct {
-							x int
-							y int
-						}{i - 1, start}] = num
-					}
-					if (j <= right && j != left) && CheckUpLeft(i, j, lines) {
-						start, num, err := GetNumber(j-1, lines[i-1])
-						// fmt.Println("num: ", num)
-						utils.CheckErr(err)
-						validNumbers[struct {
-							x int
-							y int
-						}{i - 1, start}] = num
-					}
-				}
-				if j >= left && j != right {
-					if CheckRight(i, j, lines) {
-						start, num, err := GetNumber(j+1, l)
-						// fmt.Println("num: ", num)
-						utils.CheckErr(err)
-						validNumbers[struct {
-							x int
-							y int
-						}{i, start}] = num
-					}
-				}
-				if j <= right && j != left {
-					if CheckLeft(i, j, lines) {
-						start, num, err := GetNumber(j-1, l)
-						// fmt.Println("num: ", num)
-						utils.CheckErr(err)
-						validNumbers[struct {
-							x int
-							y int
-						}{i, start}] = num
-					}
-				}
-			}
-		}
-	}
-	// fmt.Println("Valid num: ", validNumbers)
-	for _, v := range validNumbers {
-		ans += v
-	}
-	fmt.Println("\n2023/3 part1 ans: ", ans)
-	return ans
+type Point struct {
+	X int
+	Y int
 }
 
 var (
@@ -122,6 +21,25 @@ var (
 	bottom int
 )
 
+func Part1(lines []string) int {
+	initializeBounds(lines)
+	validNumbers := make(map[Point]int)
+
+	for i, l := range lines {
+		for j, c := range l {
+			if !unicode.IsDigit(c) && c != '.' {
+				processCursor(i, j, l, lines, validNumbers)
+			}
+		}
+	}
+
+	for _, v := range validNumbers {
+		ans += v
+	}
+	fmt.Println("\n2023/3 part1 ans: ", ans)
+	return ans
+}
+
 func Part2(lines []string) int {
 	ans = 0
 	top = 0
@@ -131,6 +49,67 @@ func Part2(lines []string) int {
 
 	fmt.Println("\n2023/3 part2 ans: ", ans)
 	return ans
+}
+
+func initializeBounds(lines []string) {
+	top = 0
+	right = len(lines[0]) - 1
+	left = 0
+	bottom = len(lines) - 1
+	ans = 0
+}
+
+func processCursor(i, j int, line string, lines []string, validNumbers map[Point]int) {
+	checkAndAddNumber := func(x, y int, line string) {
+		start, num, err := GetNumber(y, line)
+		utils.CheckErr(err)
+		validNumbers[Point{x, start}] = num
+	}
+
+	if i >= top && i != bottom {
+		if CheckBelow(i, j, lines) {
+			checkAndAddNumber(i+1, j, lines[i+1])
+		}
+		if j >= left && j != right && CheckDownRight(i, j, lines) {
+			checkAndAddNumber(i+1, j+1, lines[i+1])
+		}
+		if j <= right && j != left && CheckDownLeft(i, j, lines) {
+			checkAndAddNumber(i+1, j-1, lines[i+1])
+		}
+	}
+	if i <= bottom && i != top {
+		if CheckAbove(i, j, lines) {
+			checkAndAddNumber(i-1, j, lines[i-1])
+		}
+		if j >= left && j != right && CheckUpRight(i, j, lines) {
+			checkAndAddNumber(i-1, j+1, lines[i-1])
+		}
+		if j <= right && j != left && CheckUpLeft(i, j, lines) {
+			checkAndAddNumber(i-1, j-1, lines[i-1])
+		}
+	}
+	if j >= left && j != right && CheckRight(i, j, lines) {
+		checkAndAddNumber(i, j+1, line)
+	}
+	if j <= right && j != left && CheckLeft(i, j, lines) {
+		checkAndAddNumber(i, j-1, line)
+	}
+}
+
+func GetNumber(y int, line string) (int, int, error) {
+	if y < 0 || y >= len(line) {
+		return 0, 0, fmt.Errorf("index out of range")
+	}
+
+	start, end := y, y
+	for ; start > 0 && unicode.IsDigit(rune(line[start-1])); start-- {
+	}
+	for ; end < len(line)-1 && unicode.IsDigit(rune(line[end+1])); end++ {
+	}
+
+	numberStr := line[start : end+1]
+	n, err := strconv.Atoi(numberStr)
+	return start, n, err
 }
 
 func CheckDownRight(x, y int, lines []string) bool {
@@ -162,30 +141,5 @@ func CheckAbove(x, y int, lines []string) bool {
 }
 
 func CheckBelow(x, y int, lines []string) bool {
-	if x >= top && x != bottom {
-		return unicode.IsDigit(rune(lines[x+1][y]))
-	}
-	return false
-}
-
-func GetNumber(y int, line string) (int, int, error) {
-	// fmt.Printf("y: %v | line: %v\n", y, line)
-	if y < 0 || y >= len(line) {
-		return 0, 0, fmt.Errorf("index out of range")
-	}
-
-	// Find the start of the number
-	start := y
-	for start > 0 && unicode.IsDigit(rune(line[start-1])) {
-		start--
-	}
-
-	// Find the end of the number
-	end := y
-	for end < len(line)-1 && unicode.IsDigit(rune(line[end+1])) {
-		end++
-	}
-	numberStr := line[start : end+1]
-	n, err := strconv.Atoi(numberStr)
-	return start, n, err
+	return unicode.IsDigit(rune(lines[x+1][y]))
 }
