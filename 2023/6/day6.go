a072655c4e67535839e58711d98af57c204fb6a8
molson82 <wvandebelt82@yahoo.com>
2023-12-06 13:32:00 -0500

    2 ⭐

--- 2023/6/day6.go
+++ 2023/6/day6.go
@@ -1,10 +1,38 @@
 package day6
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/molson82/adventofcode/pkg/utils"
+)
+
+type Race struct {
+	Time     int
+	Distance int
+}
 
 func Part1(lines []string) int {
-	var ans int
-	// code here...
+	ans := 1
+	races := GetRaces(lines)
+	winList := []int{}
+	for _, r := range races {
+		// calc # of ways to win
+		// fmt.Println("race: ", r)
+		var wins int
+		for i := 0; i < r.Time; i++ {
+			timeLeft := r.Time - i
+			distance := timeLeft * i
+			if distance > r.Distance {
+				// fmt.Printf("Try: %v | Win: %v\n", i, distance)
+				wins++
+			}
+		}
+		winList = append(winList, wins)
+	}
+	for _, w := range winList {
+		ans *= w
+	}
 	fmt.Println("2023/6 part1 ans: ", ans)
 	return ans
 }
@@ -14,4 +42,18 @@ func Part2(lines []string) int {
 	// code here...
 	fmt.Println("2023/6 part2 ans: ", ans)
 	return ans
+}
+
+func GetRaces(lines []string) []Race {
+	var races []Race
+	times := strings.Fields(strings.Split(lines[0], "Time: ")[1])
+	distance := strings.Fields(strings.Split(lines[1], "Distance: ")[1])
+
+	for i := 0; i < len(times); i++ {
+		newRace := Race{}
+		newRace.Time = utils.Str_to_Int(times[i])
+		newRace.Distance = utils.Str_to_Int(distance[i])
+		races = append(races, newRace)
+	}
+	return races
 }
