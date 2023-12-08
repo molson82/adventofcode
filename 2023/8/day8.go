db797a6eb8f91f4ec56ce72cd4c433e5cebbc32e
molson82 <wvandebelt82@yahoo.com>
2023-12-08 12:20:22 -0500

    12 ⭐

--- 2023/8/day8.go
+++ 2023/8/day8.go
@@ -1,17 +1,66 @@
 package day8
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Part1(lines []string) int {
 	var ans int
-	// code here...
-	fmt.Println("2023/8 part1 ans: ", ans)
-	return ans
+	seq := strings.Split(lines[0], "")
+
+	network := make(map[string][]string)
+	for i := 2; i < len(lines); i++ {
+		l := lines[i]
+		spl := strings.Split(l, "= ")
+		key := strings.Fields(spl[0])[0]
+		valSpl := strings.Split(spl[1], ", ")
+		leftVal := valSpl[0][1:]
+		rightVal := valSpl[1][:len(valSpl[1])-1]
+
+		network[key] = []string{leftVal, rightVal}
+	}
+
+	// go over seq
+	stepKey := "AAA"
+	for i := 0; i < len(seq); i++ {
+		ans++
+		if stepKey == "ZZZ" {
+			break
+		}
+		s := seq[i]
+		currNode := network[stepKey]
+		if s == "R" {
+			stepKey = currNode[1]
+		} else {
+			stepKey = currNode[0]
+		}
+		// reset loop
+		if i == len(seq)-1 {
+			i = -1
+		}
+	}
+	fmt.Println("2023/8 part1 ans: ", ans-1)
+	return ans - 1
 }
 
 func Part2(lines []string) int {
 	var ans int
-	// code here...
+	seq := strings.Split(lines[0], "")
+	fmt.Println("seq: ", seq)
+
+	network := make(map[string][]string)
+	for i := 2; i < len(lines); i++ {
+		l := lines[i]
+		spl := strings.Split(l, "= ")
+		key := strings.Fields(spl[0])[0]
+		valSpl := strings.Split(spl[1], ", ")
+		leftVal := valSpl[0][1:]
+		rightVal := valSpl[1][:len(valSpl[1])-1]
+
+		network[key] = []string{leftVal, rightVal}
+	}
+
 	fmt.Println("2023/8 part2 ans: ", ans)
 	return ans
 }
--- 2023/8/day8_test.go
+++ 2023/8/day8_test.go
@@ -25,3 +25,26 @@ func TestPart1Sample2(t *testing.T) {
 	ans := day8.Part1(input)
 	assert.Equal(t, ans, 6)
 }
+
+func TestPart1(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	ans := day8.Part1(input)
+	assert.Equal(t, ans, 12083)
+}
+
+func TestPart2Sample(t *testing.T) {
+	input := []string{
+		"LR",
+		"",
+		"11A = (11B, XXX)",
+		"11B = (XXX, 11Z)",
+		"11Z = (11B, XXX)",
+		"22A = (22B, XXX)",
+		"22B = (22C, 22C)",
+		"22C = (22Z, 22Z)",
+		"22Z = (22B, 22B)",
+		"XXX = (XXX, XXX)",
+	}
+	ans := day8.Part2(input)
+	assert.Equal(t, ans, 6)
+}
