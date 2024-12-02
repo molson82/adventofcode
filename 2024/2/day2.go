0ca429a7071d088b93d9ea17d11f42afc7792bfe
molson82 <wvandebelt82@yahoo.com>
2024-12-02 17:54:56 -0500

    day 2 init :tada:

--- 2024/2/day2.go
+++ 2024/2/day2.go
@@ -2,14 +2,18 @@ package day2
 
 import "fmt"
 
-func Part1() {
+func Part1(input []string) int {
 	var ans int
 	// code here...
 	fmt.Println("2024/2 part1 ans: ", ans)
+
+	return ans
 }
 
-func Part2() {
+func Part2(input []string) int {
 	var ans int
 	// code here...
 	fmt.Println("2024/2 part2 ans: ", ans)
+
+	return ans
 }
--- 2024/2/day2_test.go
+++ 2024/2/day2_test.go
@@ -3,9 +3,31 @@ package day2_test
 import (
 	"testing"
 
+	day2 "github.com/molson82/adventofcode/2024/2"
+	"github.com/molson82/adventofcode/pkg/utils"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPart1(t *testing.T) {
-	assert.True(t, true, "true is true")
+func TestPart1Sample(t *testing.T) {
+	sample := utils.ReadInput("./sample.txt")
+	ans := day2.Part1(sample)
+	assert.Equal(t, 2, ans)
 }
+
+// func TestPart1(t *testing.T) {
+// 	input := utils.ReadInput("./input.txt")
+// 	ans := day2.Part1(input)
+// 	assert.Equal(t, 0, ans)
+// }
+//
+// func TestPart2Sample(t *testing.T) {
+// 	sample := utils.ReadInput("./sample.txt")
+// 	ans := day2.Part2(sample)
+// 	assert.Equal(t, 0, ans)
+// }
+//
+// func TestPart2(t *testing.T) {
+// 	input := utils.ReadInput("./input.txt")
+// 	ans := day2.Part2(input)
+// 	assert.Equal(t, 0, ans)
+// }
