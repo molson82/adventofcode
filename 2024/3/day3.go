3241b8ef95d16402c62857a35b39689e767d951b
molson82 <wvandebelt82@yahoo.com>
2024-12-03 12:42:29 -0500

    2024/3/part1

--- 2024/3/day3.go
+++ 2024/3/day3.go
@@ -1,10 +1,22 @@
 package day3
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
 func Part1(input []string) int {
 	var ans int
-	// code here...
+
+	for _, l := range input {
+		nums := extractNumbersPart1(l)
+		for _, n := range nums {
+			mul := n[0] * n[1]
+			ans += mul
+		}
+	}
+
 	fmt.Println("2024/3 part1 ans: ", ans)
 
 	return ans
@@ -16,4 +28,21 @@ func Part2(input []string) int {
 	fmt.Println("2024/3 part2 ans: ", ans)
 
 	return ans
+}
+
+func extractNumbersPart1(input string) [][]int {
+	// Define the regex pattern
+	pattern := `mul\((\d{1,3}),(\d{1,3})\)`
+	re := regexp.MustCompile(pattern)
+	// Find all matches
+	matches := re.FindAllStringSubmatch(input, -1)
+	// Extract the numbers from each match
+	var results [][]int
+	for _, match := range matches {
+		num1, _ := strconv.Atoi(match[1])
+		num2, _ := strconv.Atoi(match[2])
+		results = append(results, []int{num1, num2})
+	}
+
+	return results
 }
--- 2024/3/day3_test.go
+++ 2024/3/day3_test.go
@@ -12,14 +12,16 @@ func TestPart1Sample(t *testing.T) {
 	sample := utils.ReadInput("./sample.txt")
 	ans := day3.Part1(sample)
 	assert.NotEqual(t, 0, ans)
+	assert.Equal(t, 161, ans)
 }
 
-// func TestPart1(t *testing.T) {
-// 	input := utils.ReadInput("./input.txt")
-// 	ans := day3.Part1(input)
-// 	assert.NotEqual(t, 0, ans)
-// }
-//
+func TestPart1(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	ans := day3.Part1(input)
+	assert.NotEqual(t, 27499548, ans, "too low")
+	assert.Equal(t, 169021493, ans)
+}
+
 // func TestPart2Sample(t *testing.T) {
 // 	sample := utils.ReadInput("./sample.txt")
 // 	ans := day3.Part2(sample)
