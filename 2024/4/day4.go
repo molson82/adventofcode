7dbe416ff13c7e23ed0f2a0896ecaf70ec95694e
molson82 <wvandebelt82@yahoo.com>
2024-12-04 18:20:49 -0500

    2024/4 part1 wip

--- 2024/4/day4.go
+++ 2024/4/day4.go
@@ -15,6 +15,7 @@ type Row []Letter
 type Grid []Row
 
 var xLocations [][]int
+var confirmedLocations [][]int
 
 const XMAS = "XMAS"
 
@@ -22,18 +23,12 @@ func Part1(input []string) int {
 	var ans int
 
 	letterGrid := createGrid(input)
-	fmt.Println(xLocations)
 	for _, v := range xLocations {
-		if letterGrid.checkHorizontal(v[0], v[1], XMAS) {
-			ans++
-		}
-		if letterGrid.checkVertical(v[0], v[1], XMAS) {
-			ans++
-		}
-		if letterGrid.checkDiagonal(v[0], v[1], XMAS) {
-			ans++
-		}
+		ans += letterGrid.checkHorizontal(v[0], v[1], XMAS)
+		ans += letterGrid.checkVertical(v[0], v[1], XMAS)
+		ans += letterGrid.checkDiagonal(v[0], v[1], XMAS)
 	}
+	// letterGrid.printConfirmedLocations()
 
 	fmt.Println("2024/4 part1 ans: ", ans)
 
@@ -46,6 +41,26 @@ func Part2(input []string) int {
 	fmt.Println("2024/4 part2 ans: ", ans)
 
 	return ans
+}
+
+func (g Grid) printConfirmedLocations() {
+	fmt.Printf("\n")
+	for y, r := range g {
+		for x := range r {
+			var printx bool
+			// check if x,y is in confirmedLocations
+			for _, v := range confirmedLocations {
+				if v[0] == x && v[1] == y {
+					fmt.Print("X")
+					printx = true
+				}
+			}
+			if !printx {
+				fmt.Print(".")
+			}
+		}
+		fmt.Printf("\n")
+	}
 }
 
 func createGrid(input []string) Grid {
@@ -71,40 +86,133 @@ func createGrid(input []string) Grid {
 	return newGrid
 }
 
-func (g Grid) checkHorizontal(x, y int, w string) bool {
+func (g Grid) checkHorizontal(x, y int, w string) int {
+	var matchf, matchb string
+	var total int
+	wl := len(w) - 1
+
 	// check forward
-	if x+len(w) > len(g[y]) {
-		return false
-	}
-	var matchf string
-	for i := range w {
-		matchf += g[y][x+i].L
-	}
-	if matchf == w {
-		fmt.Printf("match horz forward: x: %v | y: %v\n", x, y)
-		return true
+	if x+len(w) <= len(g[y]) {
+		for i := range w {
+			matchf += g[y][x+i].L
+		}
+		if matchf == w {
+			// fmt.Printf("match horz forward: x: %v | y: %v\n", x, y)
+			addToConfirmedLocations(x, y)
+			total++
+		}
 	}
 
 	// check backward
-	if x-len(w) < 0 {
-		return false
-	}
-	var matchb string
-	for i := range w {
-		matchb += g[y][x-i].L
-	}
-	if matchb == w {
-		fmt.Printf("match horz backward: x: %v | y: %v\n", x, y)
-		return true
+	if x-wl >= 0 {
+		for i := range w {
+			matchb += g[y][x-i].L
+		}
+		if matchb == w {
+			// fmt.Printf("match horz backward: x: %v | y: %v\n", x, y)
+			addToConfirmedLocations(x, y)
+			total++
+		}
 	}
 
-	return false
+	return total
 }
 
-func (g Grid) checkVertical(x, y int, w string) bool {
-	return true
+func (g Grid) checkVertical(x, y int, w string) int {
+	var matchu, matchd string
+	var total int
+	wl := len(w) - 1
+
+	// check down
+	if y+len(w) <= len(g) {
+		for i := range w {
+			matchu += g[y+i][x].L
+		}
+		if matchu == w {
+			// fmt.Printf("match vert down: x: %v | y: %v\n", x, y)
+			addToConfirmedLocations(x, y)
+			total++
+		}
+	}
+
+	// check up
+	if y-wl >= 0 {
+		for i := range w {
+			matchd += g[y-i][x].L
+		}
+		if matchd == w {
+			// fmt.Printf("match vert up: x: %v | y: %v\n", x, y)
+			addToConfirmedLocations(x, y)
+			total++
+		}
+	}
+
+	return total
 }
 
-func (g Grid) checkDiagonal(x, y int, w string) bool {
-	return true
+func (g Grid) checkDiagonal(x, y int, w string) int {
+	var matchur, matchul, matchdr, matchdl string
+	var total int
+	wl := len(w) - 1
+
+	// check up right
+	if y-wl >= 0 && x+len(w) <= len(g[y]) {
+		for i := range w {
+			matchur += g[y-i][x+i].L
+		}
+		if matchur == w {
+			// fmt.Printf("match up right: x: %v | y: %v\n", x, y)
+			addToConfirmedLocations(x, y)
+			total++
+		}
+	}
+
+	// check up left
+	if y-wl >= 0 && x-wl >= 0 {
+		for i := range w {
+			matchul += g[y-i][x-i].L
+		}
+		if matchul == w {
+			// fmt.Printf("match up left: x: %v | y: %v\n", x, y)
+			addToConfirmedLocations(x, y)
+			total++
+		}
+	}
+
+	// check down right
+	if y+len(w) <= len(g) && x+len(w) <= len(g[y]) {
+		for i := range w {
+			matchdr += g[y+i][x+i].L
+		}
+		if matchdr == w {
+			// fmt.Printf("match down right: x: %v | y: %v\n", x, y)
+			addToConfirmedLocations(x, y)
+			total++
+		}
+	}
+
+	// check down left
+	if y+len(w) <= len(g) && x-wl >= 0 {
+		for i := range w {
+			matchdl += g[y+i][x-i].L
+		}
+		if matchdl == w {
+			// fmt.Printf("match down left: x: %v | y: %v\n", x, y)
+			addToConfirmedLocations(x, y)
+			total++
+		}
+	}
+
+	return total
+}
+
+func addToConfirmedLocations(x, y int) {
+	// Check if the x, y pair already exists in confirmedLocations
+	for _, loc := range confirmedLocations {
+		if loc[0] == x && loc[1] == y {
+			return
+		}
+	}
+	// Pair does not exist, so add it to confirmedLocations
+	confirmedLocations = append(confirmedLocations, []int{x, y})
 }
--- 2024/4/day4_test.go
+++ 2024/4/day4_test.go
@@ -14,12 +14,15 @@ func TestPart1Sample(t *testing.T) {
 	assert.Equal(t, 18, ans)
 }
 
-// func TestPart1(t *testing.T) {
-// 	input := utils.ReadInput("./input.txt")
-// 	ans := day4.Part1(input)
-// 	assert.Equal(t, 0, ans)
-// }
-//
+func TestPart1(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	ans := day4.Part1(input)
+	assert.NotEqual(t, 2541, ans, "too low")
+	assert.NotEqual(t, 2542, ans, "too low")
+	assert.NotEqual(t, 2546, ans, "too high")
+	assert.NotEqual(t, 2547, ans, "too high")
+}
+
 // func TestPart2Sample(t *testing.T) {
 // 	sample := utils.ReadInput("./sample.txt")
 // 	ans := day4.Part2(sample)
