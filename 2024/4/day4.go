02487d2bbc9fcd21c174629422cab1462aace35c
molson82 <wvandebelt82@yahoo.com>
2024-12-04 18:50:36 -0500

    2024/4 part1

--- 2024/4/day4.go
+++ 2024/4/day4.go
@@ -4,32 +4,22 @@ import (
 	"fmt"
 )
 
-type Letter struct {
-	L string
-	X int
-	Y int
+var directions = [][2]int{
+	{0, 1},   // right
+	{1, 0},   // down
+	{1, 1},   // diagonal down-right
+	{1, -1},  // diagonal down-left
+	{0, -1},  // left
+	{-1, 0},  // up
+	{-1, -1}, // diagonal up-left
+	{-1, 1},  // diagonal up-right
 }
-
-type Row []Letter
-
-type Grid []Row
-
-var xLocations [][]int
-var confirmedLocations [][]int
-
-const XMAS = "XMAS"
 
 func Part1(input []string) int {
 	var ans int
 
-	letterGrid := createGrid(input)
-	for _, v := range xLocations {
-		ans += letterGrid.checkHorizontal(v[0], v[1], XMAS)
-		ans += letterGrid.checkVertical(v[0], v[1], XMAS)
-		ans += letterGrid.checkDiagonal(v[0], v[1], XMAS)
-	}
-	// letterGrid.printConfirmedLocations()
-
+	grid := parseGrid(input)
+	ans = countXMAS(grid)
 	fmt.Println("2024/4 part1 ans: ", ans)
 
 	return ans
@@ -43,176 +33,41 @@ func Part2(input []string) int {
 	return ans
 }
 
-func (g Grid) printConfirmedLocations() {
-	fmt.Printf("\n")
-	for y, r := range g {
-		for x := range r {
-			var printx bool
-			// check if x,y is in confirmedLocations
-			for _, v := range confirmedLocations {
-				if v[0] == x && v[1] == y {
-					fmt.Print("X")
-					printx = true
+func isInBounds(x, y, rows, cols int) bool {
+	return x >= 0 && x < rows && y >= 0 && y < cols
+}
+
+func searchFromPosition(grid [][]rune, x, y, dx, dy int) bool {
+	word := "XMAS"
+	for i := 0; i < len(word); i++ {
+		nx, ny := x+dx*i, y+dy*i
+		if !isInBounds(nx, ny, len(grid), len(grid[0])) || grid[nx][ny] != rune(word[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func countXMAS(grid [][]rune) int {
+	count := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == 'X' {
+				for _, dir := range directions {
+					if searchFromPosition(grid, i, j, dir[0], dir[1]) {
+						count++
+					}
 				}
 			}
-			if !printx {
-				fmt.Print(".")
-			}
 		}
-		fmt.Printf("\n")
 	}
+	return count
 }
 
-func createGrid(input []string) Grid {
-	var newGrid Grid
-
-	for i, r := range input {
-		newRow := Row{}
-		for j, l := range r {
-			newLetter := Letter{
-				L: string(l),
-				X: j,
-				Y: i,
-			}
-			// add to xlocations
-			if string(l) == "X" {
-				xLocations = append(xLocations, []int{j, i})
-			}
-			newRow = append(newRow, newLetter)
-		}
-		newGrid = append(newGrid, newRow)
+func parseGrid(input []string) [][]rune {
+	grid := make([][]rune, len(input))
+	for i, line := range input {
+		grid[i] = []rune(line)
 	}
-
-	return newGrid
-}
-
-func (g Grid) checkHorizontal(x, y int, w string) int {
-	var matchf, matchb string
-	var total int
-	wl := len(w) - 1
-
-	// check forward
-	if x+len(w) <= len(g[y]) {
-		for i := range w {
-			matchf += g[y][x+i].L
-		}
-		if matchf == w {
-			// fmt.Printf("match horz forward: x: %v | y: %v\n", x, y)
-			addToConfirmedLocations(x, y)
-			total++
-		}
-	}
-
-	// check backward
-	if x-wl >= 0 {
-		for i := range w {
-			matchb += g[y][x-i].L
-		}
-		if matchb == w {
-			// fmt.Printf("match horz backward: x: %v | y: %v\n", x, y)
-			addToConfirmedLocations(x, y)
-			total++
-		}
-	}
-
-	return total
-}
-
-func (g Grid) checkVertical(x, y int, w string) int {
-	var matchu, matchd string
-	var total int
-	wl := len(w) - 1
-
-	// check down
-	if y+len(w) <= len(g) {
-		for i := range w {
-			matchu += g[y+i][x].L
-		}
-		if matchu == w {
-			// fmt.Printf("match vert down: x: %v | y: %v\n", x, y)
-			addToConfirmedLocations(x, y)
-			total++
-		}
-	}
-
-	// check up
-	if y-wl >= 0 {
-		for i := range w {
-			matchd += g[y-i][x].L
-		}
-		if matchd == w {
-			// fmt.Printf("match vert up: x: %v | y: %v\n", x, y)
-			addToConfirmedLocations(x, y)
-			total++
-		}
-	}
-
-	return total
-}
-
-func (g Grid) checkDiagonal(x, y int, w string) int {
-	var matchur, matchul, matchdr, matchdl string
-	var total int
-	wl := len(w) - 1
-
-	// check up right
-	if y-wl >= 0 && x+len(w) <= len(g[y]) {
-		for i := range w {
-			matchur += g[y-i][x+i].L
-		}
-		if matchur == w {
-			// fmt.Printf("match up right: x: %v | y: %v\n", x, y)
-			addToConfirmedLocations(x, y)
-			total++
-		}
-	}
-
-	// check up left
-	if y-wl >= 0 && x-wl >= 0 {
-		for i := range w {
-			matchul += g[y-i][x-i].L
-		}
-		if matchul == w {
-			// fmt.Printf("match up left: x: %v | y: %v\n", x, y)
-			addToConfirmedLocations(x, y)
-			total++
-		}
-	}
-
-	// check down right
-	if y+len(w) <= len(g) && x+len(w) <= len(g[y]) {
-		for i := range w {
-			matchdr += g[y+i][x+i].L
-		}
-		if matchdr == w {
-			// fmt.Printf("match down right: x: %v | y: %v\n", x, y)
-			addToConfirmedLocations(x, y)
-			total++
-		}
-	}
-
-	// check down left
-	if y+len(w) <= len(g) && x-wl >= 0 {
-		for i := range w {
-			matchdl += g[y+i][x-i].L
-		}
-		if matchdl == w {
-			// fmt.Printf("match down left: x: %v | y: %v\n", x, y)
-			addToConfirmedLocations(x, y)
-			total++
-		}
-	}
-
-	return total
-}
-
-func addToConfirmedLocations(x, y int) {
-	// Check if the x, y pair already exists in confirmedLocations
-	for _, loc := range confirmedLocations {
-		if loc[0] == x && loc[1] == y {
-			return
-		}
-	}
-	// Pair does not exist, so add it to confirmedLocations
-	confirmedLocations = append(confirmedLocations, []int{x, y})
+	return grid
 }
