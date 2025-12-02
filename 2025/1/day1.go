3fbc9eda6d11722d2c184aa11fbe8f030f097de7
molson82 <wvandebelt82@yahoo.com>
2025-12-01 22:44:10 -0500

    :star: 2025/1 :tada:

--- 2025/1/day1.go
+++ 2025/1/day1.go
@@ -1,19 +1,95 @@
 package day1
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/molson82/adventofcode/pkg/utils"
+)
 
 func Part1(input []string) int {
-	var ans int
-	// code here...
-	fmt.Println("2025/1 part1 ans: ", ans)
+	position := 50
+	totalPasses := 0
 
-	return ans
+	for _, v := range input {
+		direction := v[0]
+		amount := utils.Str_to_Int(v[1:])
+		if strings.ToUpper(string(direction)) == "L" {
+			position = rotateLeft(position, amount)
+		} else {
+			position = rotateRight(position, amount)
+		}
+		// Count when we land exactly on 0
+		if position == 0 {
+			totalPasses++
+		}
+	}
+	fmt.Println("2025/1 part1 ans: ", totalPasses)
+
+	return totalPasses
 }
 
 func Part2(input []string) int {
-	var ans int
-	// code here...
-	fmt.Println("2025/1 part2 ans: ", ans)
+	position := 50
+	totalPasses := 0
 
-	return ans
+	for _, v := range input {
+		direction := v[0]
+		amount := utils.Str_to_Int(v[1:])
+		var passes int
+		if strings.ToUpper(string(direction)) == "L" {
+			position, passes = rotateLeftWithCount(position, amount)
+		} else {
+			position, passes = rotateRightWithCount(position, amount)
+		}
+		totalPasses += passes
+	}
+	fmt.Println("2025/1 part2 ans: ", totalPasses)
+
+	return totalPasses
+}
+
+func rotateLeft(position int, amount int) int {
+	newPos := (position - amount) % 100
+	if newPos < 0 {
+		newPos += 100
+	}
+	return newPos
+}
+
+func rotateRight(position int, amount int) int {
+	return (position + amount) % 100
+}
+
+func rotateLeftWithCount(position int, amount int) (int, int) {
+	newPos := rotateLeft(position, amount)
+
+	var passes int
+	if position == 0 {
+		// Starting at 0, we only pass through it again after full rotations
+		passes = amount / 100
+	} else if amount >= position {
+		// We pass 0 at steps: position, position + 100, position + 200, etc.
+		passes = (amount-position)/100 + 1
+	}
+
+	return newPos, passes
+}
+
+func rotateRightWithCount(position int, amount int) (int, int) {
+	newPos := rotateRight(position, amount)
+
+	var passes int
+	if position == 0 {
+		// Starting at 0, we only pass through it again after full rotations
+		passes = amount / 100
+	} else {
+		// We pass 0 at steps: (100 - position), (100 - position) + 100, etc.
+		stepsToZero := 100 - position
+		if amount >= stepsToZero {
+			passes = (amount-stepsToZero)/100 + 1
+		}
+	}
+
+	return newPos, passes
 }
--- 2025/1/day1_test.go
+++ 2025/1/day1_test.go
@@ -12,22 +12,23 @@ func TestPart1Sample(t *testing.T) {
 	sample := utils.ReadInput("./sample.txt")
 	ans := day1.Part1(sample)
 	assert.NotEqual(t, 0, ans)
+	assert.Equal(t, 3, ans)
 }
 
-// func TestPart1(t *testing.T) {
-// 	input := utils.ReadInput("./input.txt")
-// 	ans := day1.Part1(input)
-// 	assert.Equal(t, 0, ans)
-// }
-//
-// func TestPart2Sample(t *testing.T) {
-// 	sample := utils.ReadInput("./sample.txt")
-// 	ans := day1.Part2(sample)
-// 	assert.Equal(t, 0, ans)
-// }
-//
-// func TestPart2(t *testing.T) {
-// 	input := utils.ReadInput("./input.txt")
-// 	ans := day1.Part2(input)
-// 	assert.Equal(t, 0, ans)
-// }
+func TestPart1(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	ans := day1.Part1(input)
+	assert.Equal(t, 1180, ans)
+}
+
+func TestPart2Sample(t *testing.T) {
+	sample := utils.ReadInput("./sample.txt")
+	ans := day1.Part2(sample)
+	assert.Equal(t, 6, ans)
+}
+
+func TestPart2(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	ans := day1.Part2(input)
+	assert.Equal(t, 6892, ans)
+}
