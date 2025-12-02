279d22807239c8cc3ed701613008eeb889782c92
molson82 <wvandebelt82@yahoo.com>
2025-12-02 10:30:33 -0500

    day2

---
+++ 2025/2/day2.go
@@ -0,0 +1,19 @@
+package day2
+
+import "fmt"
+
+func Part1(input []string) int {
+	var ans int
+	// code here...
+	fmt.Println("2025/2 part1 ans: ", ans)
+
+	return ans
+}
+
+func Part2(input []string) int {
+	var ans int
+	// code here...
+	fmt.Println("2025/2 part2 ans: ", ans)
+
+	return ans
+}
---
+++ 2025/2/day2_test.go
@@ -0,0 +1,33 @@
+package day2_test
+
+import (
+	"testing"
+
+	day2 "github.com/molson82/adventofcode/2025/2"
+	"github.com/molson82/adventofcode/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1Sample(t *testing.T) {
+	sample := utils.ReadInput("./sample.txt")
+	ans := day2.Part1(sample)
+	assert.NotEqual(t, 0, ans)
+}
+
+// func TestPart1(t *testing.T) {
+// 	input := utils.ReadInput("./input.txt")
+// 	ans := day2.Part1(input)
+// 	assert.Equal(t, 0, ans)
+// }
+//
+// func TestPart2Sample(t *testing.T) {
+// 	sample := utils.ReadInput("./sample.txt")
+// 	ans := day2.Part2(sample)
+// 	assert.Equal(t, 0, ans)
+// }
+//
+// func TestPart2(t *testing.T) {
+// 	input := utils.ReadInput("./input.txt")
+// 	ans := day2.Part2(input)
+// 	assert.Equal(t, 0, ans)
+// }
