55f5897b4df3b17faa0036730bb998883ccdeaca
molson82 <wvandebelt82@yahoo.com>
2025-12-03 09:42:49 -0500

    6 :star: | Day3 :tada:

--- 2025/3/day3.go
+++ 2025/3/day3.go
@@ -1,10 +1,17 @@
 package day3
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func Part1(input []string) int {
 	var ans int
-	// code here...
+
+	for _, bank := range input {
+		ans += maxNum(2, bank)
+	}
+
 	fmt.Println("2025/3 part1 ans: ", ans)
 
 	return ans
@@ -12,8 +19,37 @@ func Part1(input []string) int {
 
 func Part2(input []string) int {
 	var ans int
-	// code here...
+
+	for _, bank := range input {
+		ans += maxNum(12, bank)
+	}
+
 	fmt.Println("2025/3 part2 ans: ", ans)
 
 	return ans
+}
+
+func maxNum(size int, b string) int {
+	result := ""
+	startPos := 0
+
+	for i := 0; i < size; i++ {
+		endPos := len(b) - (size - i - 1)
+
+		maxDigit := byte('0')
+		maxPos := startPos
+
+		for j := startPos; j < endPos; j++ {
+			if b[j] > maxDigit {
+				maxDigit = b[j]
+				maxPos = j
+			}
+		}
+
+		result += string(maxDigit)
+		startPos = maxPos + 1
+	}
+
+	num, _ := strconv.Atoi(result)
+	return num
 }
--- 2025/3/day3_test.go
+++ 2025/3/day3_test.go
@@ -12,22 +12,23 @@ func TestPart1Sample(t *testing.T) {
 	sample := utils.ReadInput("./sample.txt")
 	ans := day3.Part1(sample)
 	assert.NotEqual(t, 0, ans)
+	assert.Equal(t, 357, ans)
 }
 
-// func TestPart1(t *testing.T) {
-// 	input := utils.ReadInput("./input.txt")
-// 	ans := day3.Part1(input)
-// 	assert.Equal(t, 0, ans)
-// }
-//
-// func TestPart2Sample(t *testing.T) {
-// 	sample := utils.ReadInput("./sample.txt")
-// 	ans := day3.Part2(sample)
-// 	assert.Equal(t, 0, ans)
-// }
-//
-// func TestPart2(t *testing.T) {
-// 	input := utils.ReadInput("./input.txt")
-// 	ans := day3.Part2(input)
-// 	assert.Equal(t, 0, ans)
-// }
+func TestPart1(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	ans := day3.Part1(input)
+	assert.Equal(t, 17316, ans)
+}
+
+func TestPart2Sample(t *testing.T) {
+	sample := utils.ReadInput("./sample.txt")
+	ans := day3.Part2(sample)
+	assert.Equal(t, 3121910778619, ans)
+}
+
+func TestPart2(t *testing.T) {
+	input := utils.ReadInput("./input.txt")
+	ans := day3.Part2(input)
+	assert.Equal(t, 171741365473332, ans)
+}
